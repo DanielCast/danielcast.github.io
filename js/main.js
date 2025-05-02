83939b834e048c35318080677727165f55a5109d
Daniel <danielcast94@libero.it>
2025-05-02 15:40:02 +0200

    mobile changes

--- js/main.js
+++ js/main.js
@@ -27,35 +27,6 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 };
 
-// document.addEventListener('DOMContentLoaded', () => {
-
-//   const hamburgerButton = Headers.querySelector('.hamburger-button')
-//   const mobileMenu = Headers.querySelector('.mobile-menu')
-
-//   hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active'))
-//   console.log(mobileMenu.classList.contains("active"))
-
-// });
-
-// fetch('Header.html')
-//   .then(response => response.text())
-//   .then(html => {
-//     let tempDiv = document.createElement('div');
-//     tempDiv.innerHTML = html;
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const hamburgerButton = data.querySelector('.hamburger-button')
-//     const mobileMenu = document.querySelector('.mobile-menu')
-//     if(hamburgerButton && mobileMenu) {
-//       document.body.appendChild(hamburgerButton)
-//       document.body.appendChild(mobileMenu)
-//     }
-
-//     hamburgerButton.addEventListener('click', (e) => mobileMenu.classList.toggle('active'))
-//     console.log(mobileMenu.classList.contains("active"))
-
-
-// });
 
 const divHeader = document.getElementById('header')
 
@@ -68,5 +39,6 @@ divHeader.addEventListener('click', function(e)  {
     document.querySelector('.mobile-menu').classList.toggle('active');
   }
 });
+
 
 // -------------> TODO Implement the hamburger menu for the mobile menu also in Works <--------------
